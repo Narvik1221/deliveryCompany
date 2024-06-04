e6403bc6f46eaa3d58aca3b6cf3bad7399af1af3
Narvik1221 <107913962+Narvik1221@users.noreply.github.com>
2024-06-04 23:56:23 +0600

    123

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,6 +7,32 @@ import { observer } from "mobx-react-lite";
 import { AUTH_ROUTE, MAIN_ROUTE } from "../utils/consts";
 import { useLocation } from "react-router-dom";
 export const Footer = () => {
+
+
+  const whatsapp=()=>{
+      const phoneNumber = "+79143809996"; // Замените на необходимый номер
+      const appLink = `whatsapp://send?phone=${phoneNumber}`;
+      const webLink = `https://web.whatsapp.com/send?phone=${phoneNumber}`;
+      const timeout = 1500;
+      const start = Date.now();
+    
+      // Try to open WhatsApp
+      window.location = appLink;
+    
+      // If WhatsApp is not opened in the given time, redirect to the web version
+      setTimeout(function () {
+        if (Date.now() - start < timeout + 100) {
+          window.open(webLink, "_blank");
+        }
+      }, timeout);
+  }
+  const mailSend=()=>{
+    const recipient = 'gruz_service@mail.ru'; // Replace with the actual recipient email
+    const subject = encodeURIComponent('');
+    const body = encodeURIComponent('');
+    const mailtoLink = `https://e.mail.ru/compose/?to=${recipient}&subject=${subject}&body=${body}`;
+    window.open(mailtoLink, '_blank');
+  }
   return (
     <div className="footer">
       <div className="container">
@@ -15,11 +41,11 @@ export const Footer = () => {
             <img src={logo} className="header__logo" />
           </Link>
         <div className="footer__right">
-          <a target="blank" href="whatsapp://send?phone=+73812000000" className="header__link">
+          <a onClick={whatsapp} className="header__link btn-footer" >
             <img src={what} alt="" />
-            +7 (3812) 000-000
+            +7 (914) 380 99-96
           </a>
-          <a target="blank" href="mailto:gruz_service@mail.ru" className="header__link">
+          <a onClick={mailSend}  className="header__link btn-footer">
             <img src={mail} alt="" /> gruz_service@mail.ru
           </a></div>
         </div>
